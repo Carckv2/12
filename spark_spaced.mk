a9c828da9cdf95db61c8b6c5b1370cc90f65fa62
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-02-28 16:17:19 -0500

    Update and rename xdroid_spaced.mk to spark_spaced.mk

--- spark_spaced.mk
+++ spark_spaced.mk
@@ -22,23 +22,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/xdroid/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Build configuration
 IS_PHONE := true
 TARGET_SUPPORT_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-XDROID_BUILD_TYPE := UNOFFICIAL
-XDROID_MAINTAINER := NINJA
+SPARK_BUILD_TYPE := UNOFFICIAL
+SPARK_MAINTAINER := NINJA
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xdroid_spaced
+PRODUCT_NAME := spark_spaced
 PRODUCT_DEVICE := spaced
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme Narzo 50
-PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := RMX3151
+PRODUCT_MODEL := RMX3151
+PRODUCT_MANUFACTURER := RMX3151
 
 # Build info
 BUILD_FINGERPRINT := "realme/RMX3151/RE54B4L1:11/RP1A.200720.011/1634223691782:user/release-keys"
