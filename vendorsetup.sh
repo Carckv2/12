930edc43aa4481e0412f83aa7a18476d46b79c3a
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-02-28 22:57:58 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,6 +15,3 @@ git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
 
 # packages/modules/Bluetooth (required for sysbta)
 git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
-
-# Clang 11
-git clone https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-r383902 --depth=1
