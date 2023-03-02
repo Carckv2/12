1e8d1abcd5f47cb5a6bde0979a4b50df3391e3f1
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-02 16:29:38 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -152,4 +152,3 @@ PRODUCT_PACKAGES += \
 
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/spaced/spaced-vendor.mk)
-$(call inherit-product, vendor/realme/ims-spaced/mtk-ims.mk)
