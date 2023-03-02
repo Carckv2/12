18cf066a7141576b2b9b0de31b27b77bc6bc6e24
Carckv2 <Ankitshresthaas2@mail.com>
2023-03-02 17:18:52 -0500

    first

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Vendor tree
+git clone https://github.com/goldfish07/android_vendor_samsung_j8y18lte.git vendor/samsung/j8y18lte
+
+# Kernel tree
+git clone --depth=1 git@github.com:Carckv2/android_kernel_samsung_j8y18lte.git -b lineage-18.1 kernel/samsung/j8y18lte
