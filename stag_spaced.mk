a92fb925a9216b90dbb06d82ce1e582f044deadd
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-03 07:46:19 -0500

    Update stag_spaced.mk

--- stag_spaced.mk
+++ stag_spaced.mk
@@ -22,19 +22,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/stag/main.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Build configuration
 IS_PHONE := true
 TARGET_SUPPORT_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-STAG_BUILD_TYPE := UNOFFICIAL
-STAG_MAINTAINER := NINJA
-STAG_WITH_GAPPS := true
+AICP_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := stag_spaced
+PRODUCT_NAME := aicp_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_BRAND := RM6781
 PRODUCT_MODEL := RM6781
@@ -48,3 +47,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-mp8tc16sppr10V1-release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="NINJA"
