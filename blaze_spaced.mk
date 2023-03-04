940702ecc1f60abf4994dbe8d9692f962db4be7d
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-04 13:25:16 -0500

    Update and rename corvus_spaced.mk to blaze_spaced.mk

--- blaze_spaced.mk
+++ blaze_spaced.mk
@@ -22,18 +22,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common YAAP stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Build configuration
 IS_PHONE := true
 TARGET_SUPPORT_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1480
 TARGET_GAPPS_ARCH := arm64
-RAVEN_LAIR_BUILD_TYPE := UNOFFICIAL
+BLAZE_BUILD_TYPE := UNOFFICIAL
 WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := corvus_spaced
+PRODUCT_NAME := blaze_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_BRAND := RM6781
 PRODUCT_MODEL := RM6781
