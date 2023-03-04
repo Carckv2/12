edd7c2f14d3c35cdc4232c770f7e08d65ee3d4d8
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-04 08:52:44 -0500

    Update and rename yaap_spaced.mk to derp_spaced.mk

--- derp_spaced.mk
+++ derp_spaced.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Build configuration
 IS_PHONE := true
@@ -33,7 +33,7 @@ YAAP_BUILD_TYPE := UNOFFICIAL
 WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_spaced
+PRODUCT_NAME := derp_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_BRAND := RM6781
 PRODUCT_MODEL := RM6781
